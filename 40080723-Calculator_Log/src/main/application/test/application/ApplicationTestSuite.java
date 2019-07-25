8f66c1453dbdb968bb4804bd8643f28cbc3f8968
Jemish Paghadar <47541965+Jemish27121997@users.noreply.github.com>
2019-07-25 14:40:12 -0400

    Test cases added

---
+++ 40080723-Calculator_Log/src/main/application/test/application/ApplicationTestSuite.java
@@ -0,0 +1,19 @@
+package test.application;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import main.application.MainViewController;
+/**
+ * 
+ * @author Jemish
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({MainViewController.class})
+public class ApplicationTestSuite{
+    
+}
+
+
